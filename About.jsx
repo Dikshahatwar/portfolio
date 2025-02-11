dc26cd033092b3c09f9ec2a3df3ce0dd58e14f73
Dikshahatwar <dikshahatwar16@gmail.com>
2025-02-11 17:19:01 +0530

    Update About.jsx

--- About.jsx
+++ About.jsx
@@ -5,16 +5,14 @@ import image from "../images/motion-background.jpg";
 const imageAltText = "purple and blue abstract background";
 
 const description =
-  "I'm a Web development student studying at RTMNU University. I enjoy creating unique website and simplistic user interfaces in creative ways.";
-
+  "I'm a Web development student studying at RTMNU University. I enjoy creating unique websites and simplistic user interfaces in creative ways.";
 
 const skillsList = [
   "Web design",
-  "Html",
-  "Css",
-  "Javascript",
+  "HTML",
+  "CSS",
+  "JavaScript",
 ];
-
 
 const detailOrQuote =
   "I'm currently an enthusiastic second-year Computer Engineering student at Rashtrasant Tukdoji Maharaj University Nagpur";
@@ -22,7 +20,7 @@ const detailOrQuote =
 const About = () => {
   return (
     <section className="padding" id="about">
-      <img className="background" src={"C:\Users\sanji\OneDrive\Documents\images\programmer girl.jpg"} alt={programmergirl} />
+      <img className="background" src={image} alt={imageAltText} />
       <div
         style={{
           backgroundColor: "white",
@@ -33,7 +31,7 @@ const About = () => {
         }}
       >
         <h2>About Myself</h2>
-        <p className="large">{"I am actively seeking an internship opportunity. Driven by a passion for technology and a genuine eagerness to learn.I'm keen to enhance my skills and gain hands-on experience to kick-start my career. I'm always ready to embrace new challenges . I'm excited to contribute my knowledge and energy to a dynamic team, while continually improving and broadening my horizons in the field of computer engineering. Let's build something great together!"}</p>
+        <p className="large">{"I am actively seeking an internship opportunity. Driven by a passion for technology and a genuine eagerness to learn, I'm keen to enhance my skills and gain hands-on experience to kick-start my career. I'm always ready to embrace new challenges. I'm excited to contribute my knowledge and energy to a dynamic team, while continually improving and broadening my horizons in the field of computer engineering. Let's build something great together!"}</p>
         <hr />
         <ul
           style={{
@@ -45,14 +43,15 @@ const About = () => {
           }}
         >
           {skillsList.map((skill) => (
-            <li key={html}>{css}</li>
+            <li key={skill}>{skill}</li>
           ))}
         </ul>
         <hr />
-       
+        <p>{detailOrQuote}</p>
       </div>
     </section>
   );
 };
 
 export default About;
+
