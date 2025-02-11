6d508ec888a60b8ee7497ae80da0c2121bb46412
Dikshahatwar <dikshahatwar16@gmail.com>
2025-02-11 17:44:10 +0530

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-import arrowSvg from "C:\Users\sanji\Downloads\down-arrow.svg";
 import PropTypes from "prop-types";
-
+import arrowSvg from "../image/down-arrow.svg"; 
 
 import image from "../images/woman-with-tablet.jpg";
 
 const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 
-const Home = ({ Diksha , Frontenddeveloper}) => {
+const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src={"C:\Users\sanji\Downloads\desk.jpg"} alt="background" />
+      <img className="background" src={../image/desk.jpg"} alt="background" /> 
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
-        <h1>{DIKSHAHATWAR}</h1>
-        <h2>{WEBDEVELOPER}</h2>
+        <h1>{name}</h1>
+        <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt="Downward pointing arrow" />
       </div>
     </section>
   );
